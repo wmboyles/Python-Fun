24affca4db14d2dda11b17117515308eb14e5ee4
William Boyles <40878829+wmboyles@users.noreply.github.com>
2018-07-08 21:25:53 -0400

    Update MinimalClicks5x5.py

--- Games/LightsOut/MinimalClicks5x5.py
+++ Games/LightsOut/MinimalClicks5x5.py
@@ -1,9 +1,16 @@
+'''
+This finds the minimal number of clicks needed to solve any 5x5 board. 
+I had previously found that it is either 15, 16, or 17 clicks.
+This program checks all boards created (and solveable) in 16 and 17 clicks.
+'''
+
 def boardModAdd(m,a):
     out=""
     for i in range(25):
         if m[i]==a[i]: out+="0"
         else: out+="1"
     return out
+
 
 def main():
     sixteenBoards, seventeenBoards = [], []
@@ -21,21 +28,21 @@ def main():
     N2 = "1101100000110110000011011"
     N3 = "0111010101110111010101110"
 
-    #Check 16 boards
+    #Check 16-click boards
     for board in sixteenBoards:
         S1 = boardModAdd(board,N1).count("1")
         S2 = boardModAdd(board,N2).count("1")
         S3 = boardModAdd(board,N3).count("1")
         if S1>15 and S2>15 and S3>15: print(board)
-    print("All 16 boards checked")
+    print("All 16-click boards checked")
 
-    #Check 17 boards
+    #Check 17-click boards
     for board in seventeenBoards:
         S1 = boardModAdd(board,N1).count("1")
         S2 = boardModAdd(board,N2).count("1")
         S3 = boardModAdd(board,N3).count("1")
         if S1>15 and S2>15 and S3>15: print(board)
-    print("All 17 boards checked")
+    print("All 17-click boards checked")
 
 
 
