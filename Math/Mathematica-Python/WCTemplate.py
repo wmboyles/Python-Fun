84a620f10123c15e4789456181dcc18ded16f8f6
William Boyles <40878829+wmboyles@users.noreply.github.com>
2019-04-19 17:37:25 -0400

    Added mathematica-python folder

---
+++ Math/Mathematica-Python/WCTemplate.py
@@ -0,0 +1,15 @@
+from urllib.parse import urlencode
+
+try: import urllib.request as urllib2
+except ImportError: import urllib2
+
+class WolframCloud:
+
+    def wolfram_cloud_call(self, **args):
+        arguments = dict([(key, arg) for key, arg in args.items()])
+        result = urllib2.urlopen("###Mathematica-generated URL", urlencode(arguments).encode("utf8"))
+        return result.read()
+
+    def call(self, s):
+        textresult =  self.wolfram_cloud_call(s=s)
+        return textresult
