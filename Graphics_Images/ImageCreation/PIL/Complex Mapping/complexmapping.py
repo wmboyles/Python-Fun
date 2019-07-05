32d628d357e3b2f21316c815b706746afa4fbdfe
wmboyle2 <wmboyle2@ncsu.edu>
2019-07-05 12:41:39 -0400

    Attempting to recreate mappings.

--- Graphics_Images/ImageCreation/PIL/Complex Mapping/complexmapping.py
+++ Graphics_Images/ImageCreation/PIL/Complex Mapping/complexmapping.py
@@ -4,26 +4,30 @@ import math
 
 def CxFunct(a, b):  # input a,b is z=a+bi
     z = complex(a, b)
-    return .0000000001 * math.e ** z
+    try:
+        return 10000000000*math.e**(.1/z)
+    except ZeroDivisionError:
+        return 0
 
 
-def main():
+def main(screenL, screenW): #i.e. 1366, 768 or 1920, 1080
     filename = 'img1.bmp'
-    image = Image.new('RGB', (768, 768))
+    image = Image.new('RGB', (screenL, screenW))
     size = width, height = image.size
     pixels = image.load()
 
-    a = -384
-    b = -384
+    delta = 1 #Positive Integer
 
-    for a in range(-682, 683, 1):
-        for b in range(-382, 384, 1):
+    #(0,0) at center of image
+    for a in range(1-screenL//2, screenL//2, delta):
+        for b in range(1-screenW//2, screenW//2, delta):
             
             # if((int(a/10)==a/10 or int(b/10)==b/10)): # plot a grid
-            if(int(CxFunct(a, b).real + 683) < 768 and int(CxFunct(a, b).real + 683) >= 0 and int(384 - CxFunct(a, b).imag) < 768 and int(384 - CxFunct(a, b).imag) >= 0):
-                pixels[int(CxFunct(a, b).real + 683), int(384 - CxFunct(a, b).imag)] = (255, 255, 255)  # Plot
+            c = CxFunct(a,b) #result of complex fuction
+            if((c.real + screenL//2) < screenL and int(c.real + screenL//2) >= 0 and int(screenW//2 - c.imag) < screenW and int(screenW//2 - c.imag) >= 0):
+                pixels[int(c.real + screenL//2), int(screenW//2 - c.imag)] = (255, 255, 255)  # Plot
 
-        image.save(filename)
+    image.save(filename)
     
 
-main()
+main(1000,1000)
